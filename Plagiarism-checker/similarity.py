8d7d6b696d36ed84f9234a89e3f0f347044a731a
Harshita Sahai <40337803+HarshitaSahai@users.noreply.github.com>
2020-12-16 16:54:36 +0530

    Update similarity.py

--- Plagiarism-checker/similarity.py
+++ Plagiarism-checker/similarity.py
@@ -2,10 +2,17 @@ import nltk
 import websearch
 from difflib import SequenceMatcher
 import pandas as pd
+import csv
+import numpy as np
 
 nltk.download('stopwords')
 nltk.download('punkt')
 stop_words = set(nltk.corpus.stopwords.words('english')) 
+
+df_csv1 = pd.read_csv('argument.csv',encoding='cp1252')
+arg = df_csv1.iloc[0:275,0].values
+ans = []
+links = []
 
 def purifyText(string):
     words = nltk.word_tokenize(string)
@@ -34,12 +41,50 @@ def report(text):
         matches[matching_sites[i]] = similarity(text, websearch.extractText(matching_sites[i]))
 
     matches = {k: v for k, v in sorted(matches.items(), key=lambda item: item[1], reverse=True)}
+    flag = False
     print(matches)
+    # If any link present
+    if matches:
+        # appending link + % 
+        res = matches.items()
+        data = list(res)
+        links.append(data)
+        # If greater than 30 then true
+        for i in matches:
+            if(matches[i] >= 30):
+                ans.append('1')
+                flag = True
+                break
+    # no link therefore 0 
+    else:
+        ans.append('0')
+        return matches
+    # if no link with >= 30 %
+    if flag == False:
+        ans.append('0')
 
-    return matches
 
+for i in arg:
+    print(i)
+    report(i)
 
+df_csv = pd.DataFrame(columns=['Argument'])
+df_csv1 = pd.DataFrame(columns=['Argument'])
+k = 0
+print(ans)
+print(links)
 
+# Appending 0 or 1
+for i in ans:
+    
+    df_csv.loc[k] = i 
+    k = k + 1
 
-if __name__ == '__main__':
-    report('Problems appear at midnight, Pacific Time. Youll have one day to correctly solve each problem for 10 LeetCoins. After that, you can still solve it, however, you wont get any LeetCoins. A new problem will appear each day.')
+# Appending links with %
+for i in links:
+    df_csv1 = df_csv1.append(i)
+    
+
+    
+df_csv.to_csv('sampleplargresult.csv', index=False, mode= 'w') # Sheeting have the required list
+df_csv1.to_csv('sampleplinks.csv', index=False, mode= 'w') # Sheeting have the required list
